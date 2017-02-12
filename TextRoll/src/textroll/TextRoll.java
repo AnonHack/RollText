c9731dc5ae6a2b18a4fff6aefcd593ccf4f57a0e
AnonghostHacker <lj0000.ok@163.com>
2017-02-12 23:53:20 +0800

    Add files via upload

---
+++ TextRoll/src/textroll/TextRoll.java
@@ -0,0 +1,14 @@
+package textroll;
+
+import com.lzw.StringRoll;
+
+public class TextRoll {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		StringRoll s=new StringRoll();
+		s.setVisible(true);
+		s.setText("测试中的字符串总是跑啊跑");
+	}
+
+}
